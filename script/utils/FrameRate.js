d6e7123250f83573401c8c4176401f02472d79cd
Dustin Jiang <dustinjiang@outlook.com>
2024-08-20 16:46:52 +0800

    🚀完善FPS显示

--- script/utils/FrameRate.js
+++ script/utils/FrameRate.js
@@ -28,5 +28,10 @@ class FrameRate {
         let interval = timestamp - this.startTime
 
         this.fps.innerText = Math.ceil(this.frame / (interval / 1000))
+
+        if (interval / 1000 > 5) {
+            this.frame = 0
+            this.startTime = performance.now()
+        }
     }
 }
