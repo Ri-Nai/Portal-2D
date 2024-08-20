16ea7a8353d83f71287b254c66c216659d90491a
Ri-Nai <383821630@qq.com>
2024-08-20 18:51:33 +0800

    🎨 修改一点史

--- script/Entity.js
+++ script/Entity.js
@@ -1,3 +1,9 @@
 class Entity {
+    constructor(position, size, velocity = new Vector(0, 0), acceleration = new Vector(0, 0)) {
+        this.position = position;  // 实体的当前位置
+        this.velocity = velocity;  // 实体的速度
+        this.acceleration = acceleration;  // 实体的加速度
+        this.hitbox = new Hitbox(position, size);  // 实体的碰撞盒
+    }
 
 }
--- script/Player.js
+++
@@ -1,3 +0,0 @@
-class Player extends Entity {
-
-}
