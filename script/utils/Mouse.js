67c645b8e3c9edca07c333aefeeb1f512e9a9d39
Dustin Jiang <dustinjiang@outlook.com>
2024-08-23 12:49:07 +0800

    🖱微调光标样式

--- script/utils/Mouse.js
+++ script/utils/Mouse.js
@@ -97,7 +97,9 @@ class MouseManager {
     }
 
     draw() {
+        window.$game.ctx.fillStyle = 'white';
+        window.$game.ctx.fillRect(this.x, this.y, 6, 6);
         window.$game.ctx.fillStyle = 'black';
-        window.$game.ctx.fillRect(this.x, this.y, 5, 5);
+        window.$game.ctx.fillRect(this.x + 1, this.y + 1, 4, 4);
     }
 }
