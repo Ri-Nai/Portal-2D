3c5023fd089013c12ffe57c3681d51e58c1f77b4
Ri-Nai <383821630@qq.com>
2024-08-23 18:01:19 +0800

    🚪新建传送门

--- Docs/DataStructure.md
+++ Docs/DataStructure.md
@@ -1,17 +1,17 @@
 ## MapEditor
 
-使用扫描线的方法存储Blocks和Edges<br>
+使用扫描线的方法存储Blocks和Edges 
 
-Blocks以条状模式存储，每层会切成很多片以计算碰撞<br>
+Blocks以条状模式存储，每层会切成很多片以计算碰撞 
 
-Edges同样是以条状模式存储的，并且只存储边缘<br>
-对Edge的type存储写法是$(x + 4) \times 4 + facing$<br>
-x是在excel里填的数字的值<br>
-取出$facing$值的方法就是$type \% 4$<br>
-取出$x$值的方法就是$\lfloor{\frac{type}{4} - 4}\rfloor$<br>
+Edges同样是以条状模式存储的，并且只存储边缘 
+对Edge的type存储写法是$(x + 4) \times 4 + facing$ 
+x是在excel里填的数字的值 
+取出$facing$值的方法就是$type \% 4$ 
+取出$x$值的方法就是$\lfloor{\frac{type}{4} - 4}\rfloor$ 
 
-facing值如下图所示<br>
+facing值如下图所示 
 
 ![facing](imgs/facing.jpeg)
 ![facing](imgs/facingImage.png)
-facing值的大小体现在这边的深浅上<br>
+facing值的大小体现在这边的深浅上 
--- script/Portal.js
+++ script/Portal.js
@@ -1,3 +1,27 @@
-class Portal extends Tile{
-    constructor()
+class Portal extends Tile {
+    //传送门半径1.5格
+    static portalRadius = 1.5 * BasicSize
+    static portalWidth = 0.5 * BasicSize
+    static portalDirection = [
+        new Vector(-Portal.portalRadius, 0),
+        new Vector(0, -Portal.portalRadius),
+        new Vector(-Portal.portalRadius, -Portal.portalWidth),
+        new Vector(-Portal.portalWidth, -Portal.portalRadius)
+    ]
+    static portalSize = [
+        new Vector(2 * Portal.portalRadius, Portal.portalWidth),
+        new Vector(Portal.portalWidth, 2 * Portal.portalRadius)
+    ]
+    /**
+     *
+     * @param {number} type
+     * @param {Vector} position
+     * @param {Vector} idNumber
+     */
+    constructor(type, position, idNumber) {
+        this.exitDirection = type % 4;
+        this.idNumber = idNumber;
+        super(type, position.add(portalDirection[type & 3]), Portal.portalSize[type & 1]);
+    }
+
 }
