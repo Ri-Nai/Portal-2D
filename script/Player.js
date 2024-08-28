27aa46955e9b63857b15ad3eb98248ff45781d64
Ri-Nai <383821630@qq.com>
2024-08-28 19:29:04 +0800

    🐍增加了角色动画

--- script/Player.js
+++ script/Player.js
@@ -1,22 +1,80 @@
+class Animation {
+    static Framerate = {
+        "run" : 6,
+        "jump" : 30,
+        "fall" : 30,
+        "stand" : 8,
+    };
+    static Frames = {
+        "run" : 6,
+        "jump" : 4,
+        "fall" : 2,
+        "stand" : 1,
+    }
+    constructor() {
+        this.status = "run";
+        this.facing = 1;
+        this.frame = 1;
+        this.frameRun = 0;
+    }
+    setStatus(status, facing) {
+        if (status != this.status || facing != this.facing)
+        {
+            this.frame = 1;
+            this.frameRun = 0;
+            this.status = status;
+            this.facing = facing;
+        }
+    }
+    update(deltaTime) {
+        this.frameRun += deltaTime;
+        if (this.frameRun > Animation.Framerate[this.status])
+        {
+            ++this.frame;
+            this.frameRun = 0;
+        }
+        if (this.frame > Animation.Frames[this.status])
+            switch (this.status) {
+                case "run":
+                    this.frame = 1;
+                    break;
+                case "stand":
+                    this.frame = 1;
+                    break;
+                default:
+                    --this.frame;
+                    break;
+            }
+    }
+    getFrame() {
+        if (this.status == "run")
+            console.log(this.frame);
+        return window.$game.textureManager.getTexture(this.status, this.frame * this.facing);
+    }
+}
 class Player extends Entity {
     static PlayerSize = new Vector(1.2 * basicSize, 1.8 * basicSize);
+    static PlayerOffset = new Vector(0.7 * basicSize, 0.4 * basicSize);
     constructor(position) {
         super(position, Player.PlayerSize);
         this.facing = 1;
+        this.animation = new Animation();
     }
 
     update(deltaTime) {
         deltaTime = 60 * deltaTime / 1000;
+        let move = 0;
         this.updateXY(deltaTime,
             () => {
                 if (window.$game.dialogManager.isReading) return 0;
                 let moveLeft = window.$game.inputManager.isKeysDown([ "A", "Left" ]);
                 let moveRight = window.$game.inputManager.isKeysDown([ "D", "Right" ]);
-                let move = 0;
+                move = 0;
                 if (moveLeft)
                     this.facing = move = -1;
                 if (moveRight)
                     this.facing = move = 1;
+
                 return move;
             },
             () => {
@@ -27,7 +85,27 @@ class Player extends Entity {
             },
             true
         );
+        if (this.jumping.isJumping) {
+            this.animation.setStatus("jump", this.facing);
+        } else if (!this.isOnGround() && this.jumping.isFalling) {
+            this.animation.setStatus("fall", this.facing);
+        } else {
+            if (move) {
+                this.animation.setStatus("run", this.facing);
+            }
+            else
+                this.animation.setStatus("stand", this.facing);
+        }
+        this.animation.update(deltaTime);
         this.checkOutOfMap();
+    }
+    draw() {
+        window.$game.ctx.drawImage(
+            this.animation.getFrame(),
+            this.hitbox.position.x - Player.PlayerOffset.x,
+            this.hitbox.position.y - 2 * Player.PlayerOffset.y,
+            Player.PlayerSize.x + Player.PlayerOffset.x * 2,
+            Player.PlayerSize.y + Player.PlayerOffset.y * 2);
     }
 
 }
