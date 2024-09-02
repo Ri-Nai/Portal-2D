bfaaec30e693db6ae5b0c69e985809b15bae9295
Ri-Nai <383821630@qq.com>
2024-09-03 00:42:47 +0800

    ♻️ 修改了丑陋的代码

--- script/Events/Wire.js
+++ script/Events/Wire.js
@@ -7,17 +7,17 @@ class Wire extends GameEvent {
      * @param {Vector} size
      * @param {string[]} affect
      */
-    constructor (id, type, position, size, affect, predir, nxtdir) {
+    constructor(id, type, position, size, affect, predir, nxtdir) {
         super(id, type, position, size, affect);
         this.predir = predir;
         this.nxtdir = nxtdir;
     }
 
     // 是被动的，不需要检测碰撞
-    update() {}
+    update() { }
 
     onActivate() {
-        console.debug('Wire activated', this.activated)
+        console.debug('Wire activated', this.activated);
     }
 
     onDeactivate() {
@@ -26,25 +26,17 @@ class Wire extends GameEvent {
 
     draw() {
         let status = this.activated ? "on" : "off";
+        let texture = null;
         if (this.predir == this.nxtdir)
-            window.$game.ctx.drawImage(
-                window.$game.textureManager.getTexture("wires-straight-" + status, this.predir & 1),
-                this.hitbox.position.x,
-                this.hitbox.position.y,
-                this.hitbox.size.x,
-                this.hitbox.size.y);
+            texture = window.$game.textureManager.getTexture("wires-straight-" + status, this.predir & 1);
         else {
-            let id = ""
+            let id = "";
             if ((this.nxtdir + 1 & 3) == this.predir)
                 id = String(this.predir) + String(this.nxtdir);
             else
                 id = String(this.nxtdir + 2 & 3) + String(this.predir + 2 & 3);
-            window.$game.ctx.drawImage(
-                window.$game.textureManager.getTexture("wires-cursed-" + status, id),
-                this.hitbox.position.x,
-                this.hitbox.position.y,
-                this.hitbox.size.x,
-                this.hitbox.size.y);
+            texture = window.$game.textureManager.getTexture("wires-cursed-" + status, id);
         }
+        window.$game.ctx.drawImage(texture, this.hitbox.position.x, this.hitbox.position.y, this.hitbox.size.x, this.hitbox.size.y);
     }
 }
