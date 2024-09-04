ac5b4033c16d2d737f75556e2683e76eda6c14a9
Ri-Nai <383821630@qq.com>
2024-09-04 19:16:40 +0800

    🐛 修复了巴菲重复获得的问题，然后增加了几张CG

--- script/Events/Parfait.js
+++ script/Events/Parfait.js
@@ -1,25 +1,27 @@
 class ParfaitEvent extends GameEvent {
     constructor(id, type, position, size, affect) {
         super(id, type, position, size, affect);
-        this.exist = !this.getHistory().includes(id)
+        this.exist = !this.getHistory().includes(id);
     }
 
     onActivate() {
-        this.exist = false
-        this.setHistory(this.id)
+        if (this.exist) {
+            this.exist = false;
+            this.setHistory(this.id);
 
-        const popup = document.querySelector(".parfait");
+            const popup = document.querySelector(".parfait");
 
-        popup.querySelector(".title").innerText = this.getHistory().length;
+            popup.querySelector(".title").innerText = this.getHistory().length;
 
-        popup.classList.remove("hidden");
+            popup.classList.remove("hidden");
 
-        setTimeout(() => {
-            popup.classList.add("hide");
             setTimeout(() => {
-                popup.classList.replace("hide", "hidden");
-            }, 1000)
-        }, 5000);
+                popup.classList.add("hide");
+                setTimeout(() => {
+                    popup.classList.replace("hide", "hidden");
+                }, 1000);
+            }, 5000);
+        }
     }
 
     draw() {
@@ -39,13 +41,13 @@ class ParfaitEvent extends GameEvent {
      * @returns {string[]}
      */
     getHistory() {
-        return JSON.parse(Store.get("parfait") ?? "[]")
+        return JSON.parse(Store.get("parfait") ?? "[]");
     }
 
     setHistory(id) {
-        let history = this.getHistory()
-        if (history.includes(id)) return
-        history.push(id)
-        Store.set("parfait", JSON.stringify(history))
+        let history = this.getHistory();
+        if (history.includes(id)) return;
+        history.push(id);
+        Store.set("parfait", JSON.stringify(history));
     }
 }
--- script/utils/Splash.js
+++ script/utils/Splash.js
@@ -1,16 +1,29 @@
 class Splash {
     constructor() {
         this.ele = document.querySelector("#game .splash");
+        this.isShowing = false;
     }
 
     async showImg(src) {
+        if (this.isShowing)
+            await this.moveImg();
         const img = await window.$game.dataManager.loadImg(src);
+        this.isShowing = true;
         this.ele.appendChild(img);
         this.fadeIn()
     }
-
+    async moveImg() {
+        this.ele.classList.add("fadeOut");
+        await this.wait(500);
+        this.isShowing = false;
+        this.ele.innerHTML = "";
+        await this.wait(250);
+        this.ele.classList.add("hidden");
+        this.ele.classList.remove("fadeOut");
+    }
     async hide() {
         await this.fadeOut();
+        this.isShowing = false;
         this.ele.innerHTML = "";
     }
 
